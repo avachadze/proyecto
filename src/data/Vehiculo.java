99b49a78f5a1f033e1ef9d5f6ee3ca5764bf96cd
DW31 <ik012982i9@H012982.NET>
2020-03-11 09:57:26 +0100

    Primer commit

---
+++ src/data/Vehiculo.java
@@ -0,0 +1,10 @@
+package data;
+
+public abstract class Vehiculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+}
---
+++ src/main/Main.java
@@ -0,0 +1,45 @@
+package main;
+
+import console.Console;
+
+public class Main {
+	
+	private static void menu() {
+		int ask= 0;
+		System.out.println("Bienvenido\n");
+		do {
+			System.out.println("1: Ver stock de vehículos\n2: Gestionar pintado\n3: Gestionar venta\n4: Gestionar compra\n5: Salir del programa");
+			System.out.println("Inserta la opción necesitada");
+			ask= Console.readInt();
+			switch (ask) {
+			case 1: stockDisplay(); break;
+			case 2: pintado(); break;
+			case 3: venta(); break;
+			case 4: compra(); break;
+			case 5: System.out.println("\nPrograma cerrado"); break;
+			default: System.out.println("Número incorrecto introducido, inserta un número asignado a cualquiera de las opciones.");
+			}
+		} while(ask != 5);
+	}
+	
+	private static void stockDisplay() {
+		
+	}
+	
+	private static void pintado() {
+		
+	}
+	
+	private static void venta() {
+		
+	}
+
+	private static void compra() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		menu();
+	}
+}
