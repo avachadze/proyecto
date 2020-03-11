4989833058d49c0975ac8af1cda42712f16434d9
DW31 <ik012982i9@H012982.NET>
2020-03-11 13:30:26 +0100

    Actualización del contenido de la aplicación pre-implementación de las funciones de escritura y lectura de xml

--- src/functions/Main.java
+++ src/functions/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		int ask= 0;
 		System.out.println("Bienvenido\n");
 		do {
-			System.out.println("1: Ver stock de vehículos\n2: Gestionar pintado\n3: Gestionar venta\n4: Gestionar compra\n5: Salir del programa");
+			System.out.println("1: Ver stock de vehículos\n2: Gestionar pintado\n3: Gestionar venta\n4: Gestionar compra\n5: Obtener datos de vehiculos desde archivo XML\n6: Exportar datos de stock disponible a archivo XML\n7: Salir del programa");
 			System.out.println("\nInserta la opción necesitada");
 			ask= Console.readInt();
 			switch (ask) {
@@ -16,7 +16,9 @@ public class Main {
 			case 2: pintado(); break;
 			case 3: venta(); break;
 			case 4: compra(); break;
-			case 5: System.out.println("\nPrograma cerrado"); break;
+			case 5: lectura(); break;
+			case 6: exportar(); break;
+			case 7: System.out.println("\nPrograma cerrado"); break;
 			default: System.out.println("Número incorrecto introducido, inserta un número asignado a cualquiera de las opciones.");
 			}
 		} while(ask != 5);
@@ -35,6 +37,14 @@ public class Main {
 	}
 
 	private static void compra() {
+		
+	}
+
+	private static void lectura() {
+		
+	}
+
+	private static void exportar() {
 		
 	}
 	
